660852622b9ab6e81f50e322e0604e28241b4942
miguel <mmsuarez@uamv.edu.ni>
2026-05-06 12:22:52 -0600

    fix agregue validaciones

--- arreglo1/Program.cs
+++ arreglo1/Program.cs
@@ -6,6 +6,16 @@ double promedio = 0;
 
 for( int i = 0; i < edades.Length; i++)
 {
+    try
+    {
+         Console.WriteLine("Dime la edad " + (i + 1) + ":");
+         edades[i] = int.Parse(Console.ReadLine());
+    }
+    catch 
+    {
+        Console.WriteLine("Por favor, ingresa un número válido.");
+        i--; // para volver a pedir la misma pisicion
+    }
     Console.WriteLine("Dime la edad " + (i + 1) + ":");
     edades[i] = int.Parse(Console.ReadLine());
     suma += edades[i];
